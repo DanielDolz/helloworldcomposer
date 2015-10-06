38245f2e9f9b06caffd532e31b7a788d388bb37a
DanielDolz <danieldolz@iesebre.com>
2015-10-06 19:44:01 +0200

    Primera versio

---
+++ main.php
@@ -0,0 +1,15 @@
+<?php
+
+require __DIR__ . "/Hello.php";
+
+/**
+ * Created by PhpStorm.
+ * User: alumne
+ * Date: 06/10/15
+ * Time: 19:32
+ */
+
+$greetings = new Hello();
+
+$greetings->sayHello();
+
